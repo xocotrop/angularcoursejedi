7ce96d3d0d276468a00cc80298c42468e5b1776f
Igor Janoski dos Santos <igor.santos@ciashop.local>
2017-07-11 13:55:45 -0300

    versionamento

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,17 @@
+import { Student } from './student/student.model';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'jad-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  templateUrl: './app.component.html'
 })
 export class AppComponent {
-  title = 'jad';
+
+  students: Student[] = [
+
+  {name: 'Luke', isJedi: true, temple: 'Coruscant'},
+  {name: 'Leia', isJedi: false},
+  {name: 'Han Solo', isJedi: false}
+
+  ]
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,22 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms'
+import { HttpModule } from '@angular/http'
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { StudentComponent } from './student/student.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    StudentComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    HttpModule
   ],
   providers: [],
   bootstrap: [AppComponent]
